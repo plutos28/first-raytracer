0e4e4fa1158babfeddc6ab97198f7a22c4ed10f2
plutos28 <gamevictor28@gmail.com>
2022-12-26 09:12:30 +0300

    move tuple functions over to class and remove asserts

--- RayTracerChallenge/RayTracerChallenge.cpp
+++ RayTracerChallenge/RayTracerChallenge.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <assert.h>
 #include <cstdio>
 
 #include "tuple.h"
@@ -8,12 +6,6 @@ void run_tests();
 
 int main()
 {
-    Tuple a;
-    a.x = 4.3;
-    a.y = -4.2;
-    a.z = 3.1;
-    a.w = 1.0;
-
     // Run all the tests
     run_tests();
 
--- RayTracerChallenge/tuple.h
+++ RayTracerChallenge/tuple.h
@@ -1,27 +1,36 @@
 #pragma once
-#include <assert.h>
 
-struct Tuple {
+struct TupleData {
     double x;
     double y;
     double z;
     double w;
 };
 
-bool tuple_is_point(Tuple a) {
-    return a.w == 1.0;
-}
+class Tuple {
+public:
+    static bool isPoint(TupleData a) {
+        return a.w == 1.0;
+    }
+};
 
-void test_tuple_is_point() {
-    Tuple a;
+
+void testTupleisPoint() {
+    TupleData a;
     a.x = 4.3;
     a.y = -4.2;
     a.z = 3.1;
     a.w = 1.0;
 
-    assert(tuple_is_point(a));
+    if (a.w == 1.0) {
+        printf("Test Passed: testTupleisPoint");
+    }
+    else {
+        printf("Test Failed: testTupleisPoint");
+    }
+    
 }
 
 void run_tuple_tests() {
-    test_tuple_is_point();
+    testTupleisPoint();
 }
