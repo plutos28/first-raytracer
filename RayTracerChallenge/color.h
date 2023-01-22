2943d09a5a5301545dfd918e5dbbf040ff75cdb1
plutos28 <gamevictor28@gmail.com>
2023-01-22 10:38:07 +0300

    add subtract and multiply

--- RayTracerChallenge/color.h
+++ RayTracerChallenge/color.h
@@ -20,6 +20,14 @@ public:
     Color add(Color c2) {
         return Color(red + c2.red, green + c2.green, blue + c2.blue);
     }
+
+    Color subtract(Color c2) {
+        return Color(red - c2.red, green - c2.green, blue - c2.blue);
+    }
+
+    Color multiply(double scalar) {
+        return Color(red * scalar, green * scalar, blue * scalar);
+    }
 };
 
 std::ostream& operator<<(std::ostream& s, const Color& c) {
@@ -54,8 +62,62 @@ void testAddColors() {
     }
 }
 
+void testSubtractColors() {
+    Color c1(0.9, 0.6, 0.75); 
+    Color c2(0.7, 0.1, 0.25); 
+    Color expectedColor(0.2, 0.5, 0.5);
+    bool testPassed = false;
+
+    Color resultColor = c1.subtract(c2); 
+
+    if(resultColor.equal(expectedColor)) {
+        testPassed = true;
+    }
+
+    if (testPassed) {
+        colorTestResults.passed += 1;
+        std::cout << "Test Passed: testSubtractColors\n";
+        std::cout << "\tResult: " << resultColor << "\n";
+        std::cout << "\tExpected: " << expectedColor << "\n";
+    }
+    else {
+        colorTestResults.failed += 1;
+        std::cout << "Test Failed: testSubtractColors\n";
+        std::cout << "\tResult: " << resultColor << "\n";
+        std::cout << "\tExpected: " << expectedColor << "\n";
+    }
+}
+
+void testMultiplyColorByScalar() {
+    Color c1(0.2, 0.3, 0.4); 
+    double scalar = 2;
+    Color expectedColor(0.4, 0.6, 0.8);
+    bool testPassed = false;
+
+    Color resultColor = c1.multiply(scalar);
+
+    if(resultColor.equal(expectedColor)) {
+        testPassed = true;
+    }
+
+    if (testPassed) {
+        colorTestResults.passed += 1;
+        std::cout << "Test Passed: testMultiplyColorByScalar\n";
+        std::cout << "\tResult: " << resultColor << "\n";
+        std::cout << "\tExpected: " << expectedColor << "\n";
+    }
+    else {
+        colorTestResults.failed += 1;
+        std::cout << "Test Failed: testMultiplyColorByScalar\n";
+        std::cout << "\tResult: " << resultColor << "\n";
+        std::cout << "\tExpected: " << expectedColor << "\n";
+    }
+}
+
 void run_color_tests() {
     testAddColors();
+    testSubtractColors();
+    testMultiplyColorByScalar();
 
     // print out the percentage that have passed 
     unsigned int totalTests = colorTestResults.passed + colorTestResults.failed;
